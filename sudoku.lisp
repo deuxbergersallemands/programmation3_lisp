8fd8bb71afd088125bb8465073820bdcd9c8eef2
Troy Campbell <tcampbell@emi.u-bordeaux1.fr>
2015-11-02 19:22:35 +0100

    Boucle simple implémentée, etc.

--- sudoku.lisp
+++ sudoku.lisp
@@ -1,5 +1,8 @@
 (defparameter *solution-liste* (list (list 2 4 5 1 7 9 3 8 6) (list 1 9 6 8 2 3 5 4 7) (list 8 3 7 4 6 5 2 1 9) (list 6 8 4 5 1 7 9 2 3) (list 3 2 9 6 8 4 7 5 1) (list 5 7 1 3 9 2 4 6 8) (list 4 6 2 9 3 8 1 7 5) (list 7 1 3 2 5 6 8 9 4) (list 9 5 8 7 4 1 6 3 2)))
 
+(defparameter *solution-liste-presque* (list (list 0 4 5 1 7 9 3 8 6) (list 0 9 6 8 2 3 5 4 7) (list 8 3 7 4 6 5 2 1 9) (list 6 8 4 5 1 7 9 2 3) (list 3 2 9 6 8 4 7 5 1) (list 5 7 1 3 9 2 4 6 8) (list 4 6 2 9 3 8 1 7 5) (list 7 1 3 2 5 6 8 9 4) (list 9 5 8 7 4 1 6 3 2)))
+
+
 (defparameter *solution-avec-zero* (list (list 0 4 0 0 0 0 3 0 0) (list 0 0 6 8 2 0 0 4 7) (list 0 3 0 4 0 5 0 0 9) (list 0 8 4 0 0 7 9 0 0) (list 3 2 0 0 8 0 0 5 1) (list 0 0 1 3 0 0 4 6 0) (list 4 0 0 9 0 8 0 7 0) (list 7 1 0 0 5 6 8 0 0) (list 0 0 8 0 0 0 0 3 0) ))
 
 
@@ -7,6 +10,9 @@
 (defparameter *grille-modele* (make-array '(9 9) :initial-contents *solution-avec-zero*))
 
 (defparameter *grille-modifiable* (make-array '(9 9) :initial-contents *solution-avec-zero*))
+
+(defparameter *grille-vite* (make-array '(9 9) :initial-contents *solution-liste-presque*))
+
 
 (defparameter *grille-solution* (make-array '(9 9) :initial-contents *solution-liste*))
 
@@ -23,7 +29,7 @@
 (defun demander-chiffre ()
    (defparameter *drapeau-chif* 0)    
    (loop do
-      (format t "Choissisez un chiffre entre 1 et 9: ")
+      (format t "Choisissez un chiffre entre 1 et 9: ")
       (defparameter *chiffre* (parse-integer (read-line)))
       (if (and (> *chiffre* 0) (< *chiffre* 10))
          (progn (setf *drapeau-chif* 0) 
@@ -35,7 +41,7 @@
 (defun demander-colonne ()
    (defparameter *drapeau-col* 0)
    (loop do
-      (format t "Choissisez une colonne (A-I): ")
+      (format t "Choisissez une colonne (A-I): ")
       (defparameter *colonne* (char-code (read-char)))
       (if (and (< *colonne* 74) (> *colonne* 64))
          (progn (setf *drapeau-col* 0)
@@ -47,7 +53,7 @@
 (defun demander-rang ()
    (defparameter *drapeau-rang* 0)  
    (loop do
-      (format t "Choissisez un rang (1-9): ")
+      (format t "Choisissez un rang (1-9): ")
       (defparameter *rang* (parse-integer (read-line)))
       (if (and (> *rang* 0) (< *rang* 10))
          (progn (setf *drapeau-rang* 0) 
@@ -61,7 +67,7 @@
      (setf *drapeau-placement* 0)    ; Modifiable 
      (setf *drapeau-placement* 1)))  ; Non-modifiable
 
-(defun remplir-grille (x y grille chiffre)
+(defun remplir-grille (y x grille chiffre)
    (defparameter *drapeau-placement* 0)
       (verifier-modele x y *grille-modele*)
       (if (= 0 *drapeau-placement*)
@@ -71,10 +77,34 @@
     
 
 
-;(defun verifier-solution())
+(defun verifier-solution(grille solution)
+   (defparameter *drapeau* 0)
+   (loop for y from 0 to 8 do
+     (if (/= *drapeau* 1)
+      (loop for x from 0 to 8 do
+         (if (/= *drapeau* 1)
+      (if (/= (aref grille x y) (aref solution x y))
+         (setf *drapeau* 1))))))
+   (if (/= *drapeau* 1)
+      (format t "Félicitations ! Vous avez gagné !")
+      (format t "Vous n'avez pas encore gagné ! ")))
+
+
+
    
 ;;; LE JEU ;;;
 
 (defun sudoku()
-    
-)
+   (format t "Yo ! Bienvenue ! Bienvenido ! Wilkommen !")
+   (loop do 
+      (format t "debut boucle")
+      ;(afficher-grille *grille-modifiable*)
+      (afficher-grille *grille-vite*)
+      (demander-rang)
+      ;(demander-colonne)
+      (demander-chiffre)
+      (remplir-grille 0 *rang* *grille-vite* *chiffre*)
+      (verifier-solution *grille-vite* *grille-solution*)
+      ;(remplir-grille 0 *rang* *grille-modifiable* *chiffre*)
+      ;(verifier-solution *grille-modifiable* *grille-solution*)
+     until (= *drapeau* 0)))
